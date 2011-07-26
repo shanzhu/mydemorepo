9be73f94d3215383f38f6a786bfa659a57ae0f79
wangbin <wangbin@gmail.com>
2011-07-26 15:39:44 +0800

    Add User.java version 1.0

---
+++ Mydemo/src/com/mydemo/test/User.java
@@ -0,0 +1,33 @@
+package com.mydemo.test;
+
+public class User {
+	
+	// 编号
+	private String cid;
+	
+	// 用户名
+	private String name;
+	
+	/**
+	 * 默认构造
+	 */
+	public User(){}
+
+	public String getCid() {
+		return cid;
+	}
+
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
